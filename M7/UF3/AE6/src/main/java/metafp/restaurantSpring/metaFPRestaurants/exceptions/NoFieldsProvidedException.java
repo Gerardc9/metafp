e6ea451cb99f4dcd4cb4a208953a74011479b340
Gerardc9 <gerard.c.tw@gmail.com>
2024-11-25 20:49:17 +0100

    Delete M7/UF3/AE6 directory

--- M7/UF3/AE6/src/main/java/metafp/restaurantSpring/metaFPRestaurants/exceptions/NoFieldsProvidedException.java
+++
@@ -1,7 +0,0 @@
-package metafp.restaurantSpring.metaFPRestaurants.exceptions;
-
-public class NoFieldsProvidedException extends RuntimeException{
-  public NoFieldsProvidedException(String message) {
-    super(message);
-}
-}
